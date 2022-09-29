dac3f299dd5a62105ea88741dfd839fde124d0de
Nahlenko Oleksandr <noapioss@gmail.com>
2022-09-29 18:49:39 +0300

    edit class a bit

--- TwitterClone.Api/Controllers/MakeCommentPageController.cs
+++ TwitterClone.Api/Controllers/MakeCommentPageController.cs
@@ -4,10 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 namespace TwitterClone.Api.Controllers;
 
 [Route("/posts")]
-public class MakeCommentController : BaseController
+public class MakeCommentPageController : BaseController
 {
     [HttpGet("{postId}/make-comment")]
-    public ContentResult GetLikePage([FromRoute] int postId, CancellationToken cancellationToken)
+    public ContentResult GetMakeCommentPage([FromRoute] int postId, CancellationToken cancellationToken)
     {
         var html = System.IO.File.ReadAllText("wwwroot/make-comment.html");
         html = html.Replace("{{postId}}", postId.ToString());
